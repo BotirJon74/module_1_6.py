18f37574e5f5833aa4f570377074b01068552bd4
BotirJon74 <31174@mail.ru>
2024-09-05 22:19:29 +0300

    Add files via upload

---
+++ 'module_1_6.py'.py
@@ -0,0 +1,18 @@
+# my_dict = {'Батыр':1974,'Игорь': 1980, 'Лена':1991}
+# print(my_dict)
+# print(my_dict['Лена'])
+# print(my_dict.get('Оля'))
+# my_dict.update({'Masha': 2002,
+#                 'Gena': 2005})
+# print(my_dict)
+# a=my_dict.pop('Игорь')
+# print(my_dict)
+# print(a)
+# print(my_dict)
+
+my_set = {1,2,3,4,5,2,5,4,3,6,4,2,1,"Luna","Lena","Luna",(3.14,4.15)}
+print(my_set)
+my_set.add( 8)
+my_set.add("Urban",)
+print(my_set.remove(6))
+print(my_set)
